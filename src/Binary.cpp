8c4b86a2c177aac386aad937425bee8a8f96d650
Sherry65-code <sparambir65@gmail.com>
2023-11-02 16:29:28 +0530

    Fixed time coming 0secs issue and fixed shader not compiling due to wrong path issue

--- src/Binary.cpp
+++ src/Binary.cpp
@@ -12,12 +12,12 @@ int main() {
     shader1.FragmentShader = "assets/Shader1.fs";
     engine.SetWindowParameters("Hello, World!", 800, 600);
     engine.NewWindow();
-    shaderprogram1 = engine.CompileShaders(shader1);
-    std::cout << "Compiling Shaders...";
     clock_t tStart = clock();
+    std::cout << "Compiling Shaders...";
+    shaderprogram1 = engine.CompileShaders(shader1);
     std::cout << "Compilation Over!\nTime Taken to Compile Shaders => " << (double)(clock()-tStart)/CLOCKS_PER_SEC << "secs";
     engine.UseProgram(shaderprogram1);
-    engine.SetDisplayModeToWireframe(true);
+    engine.SetDisplayModeToWireframe(false);
     engine.Loop();
     engine.DeleteProgram(shaderprogram1);
     engine.Exit();
