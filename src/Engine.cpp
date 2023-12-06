79c03accc937a05cbf36fe843f960b9cbbda27ae
Sherry65-code <sparambir65@gmail.com>
2023-12-06 12:50:03 +0530

    Added Texture Support

--- src/Engine.cpp
+++ src/Engine.cpp
@@ -1,6 +1,10 @@
 #include "Engine.hpp"
 
+#define STB_IMAGE_IMPLEMENTATION
+#include "external/stb_image.h"
+
 bool is_fullscreen = false;
+uint32_t texture;
 
 void Engine::SetWindowParameters(std::string window_name, int window_width, int window_height) {
     _window.name = window_name;
@@ -201,46 +205,80 @@ void Engine::processInput() {
 
 void Engine::before_render() {
 
-    // Define the vertices and indices
     float vertices[] = {
-        1.0f, 1.0f, 0.0f,       // top right
-        1.0f, -1.0f, 0.0f,      // bottom right
-        -1.0f, -1.0f, 0.0f,     // bottom left
-        -1.0f, 1.0f, 0.0f       // top left
-    };
-    unsigned int indices[] = {
-         3, 2, 1,       // first triangle
-         3, 0, 1        // second triangle
+        // positions          // colors           // texture coords
+         0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f, // top right
+         0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f, // bottom right
+        -0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f, // bottom left
+        -0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f  // top left 
     };
 
-    // Generate and bind the buffers
-    glGenBuffers(1, &Engine::graphics.VBO);
+    unsigned int indices[] = {
+        0, 1, 3, // first triangle
+        1, 2, 3  // second triangle
+    };
+
     glGenVertexArrays(1, &Engine::graphics.VAO);
+    glGenBuffers(1, &Engine::graphics.VBO);
     glGenBuffers(1, &Engine::graphics.EBO);
 
     glBindVertexArray(Engine::graphics.VAO);
 
-    // Bind the VBO
     glBindBuffer(GL_ARRAY_BUFFER, Engine::graphics.VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-    // Bind the EBO
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, Engine::graphics.EBO);
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
-    // Set up vertex attribute pointers
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    // position attribute
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
 
-    // Unbind the VAO
-    glBindVertexArray(0);
+    // color attribute
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
+    
+    // texture coord attribute
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+    glEnableVertexAttribArray(2);
+
+    unsigned int texture;
+
+    glGenTextures(1, &texture);
+
+    glBindTexture(GL_TEXTURE_2D, texture); // all upcoming GL_TEXTURE_2D operations now have effect on this texture object
+
+    // set the texture wrapping parameters
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);	// set texture wrapping to GL_REPEAT (default wrapping method)
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+
+    // set texture filtering parameters
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+    // load image, create texture and generate mipmaps
+    int width, height, nrChannels;
+
+    unsigned char* data = stbi_load("assets/tex/container.png", &width, &height, &nrChannels, 0);
+
+    if (data)
+    {
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+    }
+    else
+    {
+        std::cout << "Failed to load texture" << std::endl;
+    }
+
+    stbi_image_free(data);
+
 }
 
 void Engine::render() {
     glUseProgram(Engine::default_shader_program);
     glBindVertexArray(Engine::graphics.VAO);
     glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
-    glBindVertexArray(0);
 }
 
 std::string Engine::read_from_file(const std::string& filename) {
